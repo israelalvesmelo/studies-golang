93fe98a705b019116892112d2124f5f1e98b9130
Israel <israelalvesmelo@hotmail.com>
2023-02-20 19:42:04 -0300

    Added dataset test example

---
+++ testes/tabela/strings_test.go
@@ -0,0 +1,30 @@
+package tabela
+
+import (
+	"strings"
+	"testing"
+)
+
+const msgIndex = "%s (parte: %s) - Índices: esperado (%d) <> encontrado (%d)."
+
+func TestIndex(t *testing.T) {
+	testes := []struct {
+		texto    string
+		parte    string
+		esperado int
+	}{
+		{"Cod@r é show", "Cod@r", 1},
+		{"", "", 0},
+		{"Opa", "opa", -1},
+		{"leonardo", "o", 2},
+	}
+
+	for _, teste := range testes {
+		t.Logf("Massa %v", teste)
+		atual := strings.Index(teste.texto, teste.parte)
+
+		if atual != teste.esperado {
+			t.Errorf(msgIndex, teste.texto, teste.parte, teste.esperado, atual)
+		}
+	}
+}
